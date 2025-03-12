a0b425c7436037ad33b3cddf86a51119e8c8a427
flevrnght <maksyz15@gmail.com>
2025-03-12 10:32:11 +0200

    Add project files.

---
+++ CollegeInfoSystem/ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace CollegeInfoSystem.ViewModels;
+
+public partial class MainViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string _title = "Інформаційна система коледжу";
+}
