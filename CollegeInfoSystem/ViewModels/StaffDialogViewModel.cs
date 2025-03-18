5f72d3812d692227a33f8ebaac1cfa146045579a
flevrnght <maksyz15@gmail.com>
2025-03-18 18:01:38 +0200

    Add better Staff CRUD

---
+++ CollegeInfoSystem/ViewModels/StaffDialogViewModel.cs
@@ -0,0 +1,118 @@
+﻿using CollegeInfoSystem.Models;
+using CollegeInfoSystem.ViewModels;
+using CommunityToolkit.Mvvm.Input;
+
+public class StaffDialogViewModel : BaseViewModel
+{
+    private Staff _staff;
+    private Staff _originalStaff;
+
+    public Action CloseAction { get; set; }
+
+    public Staff Staff
+    {
+        get => _staff;
+        set
+        {
+            _staff = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string FirstName
+    {
+        get => _staff.FirstName;
+        set
+        {
+            _staff.FirstName = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string LastName
+    {
+        get => _staff.LastName;
+        set
+        {
+            _staff.LastName = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string Position
+    {
+        get => _staff.Position;
+        set
+        {
+            _staff.Position = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string Email
+    {
+        get => _staff.Email;
+        set
+        {
+            _staff.Email = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string Phone
+    {
+        get => _staff.Phone;
+        set
+        {
+            _staff.Phone = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public RelayCommand SaveCommand { get; }
+    public RelayCommand CancelCommand { get; }
+
+    public StaffDialogViewModel(Staff staff)
+    {
+        _staff = staff;
+        _originalStaff = new Staff
+        {
+            FirstName = staff.FirstName,
+            LastName = staff.LastName,
+            Position = staff.Position,
+            Email = staff.Email,
+            Phone = staff.Phone
+        };
+
+        SaveCommand = new RelayCommand(Save);
+        CancelCommand = new RelayCommand(Cancel);
+
+        FirstName = staff.FirstName;
+        LastName = staff.LastName;
+        Position = staff.Position;
+        Email = staff.Email;
+        Phone = staff.Phone;
+    }
+
+    public void Save()
+    {
+        _staff.FirstName = FirstName;
+        _staff.LastName = LastName;
+        _staff.Position = Position;
+        _staff.Email = Email;
+        _staff.Phone = Phone;
+
+        CloseAction?.Invoke();
+    }
+
+    public void Cancel()
+    {
+        _staff.FirstName = _originalStaff.FirstName;
+        _staff.LastName = _originalStaff.LastName;
+        _staff.Position = _originalStaff.Position;
+        _staff.Email = _originalStaff.Email;
+        _staff.Phone = _originalStaff.Phone;
+
+        CloseAction?.Invoke();
+    }
+}
--- CollegeInfoSystem/ViewModels/StaffViewModel.cs
+++ CollegeInfoSystem/ViewModels/StaffViewModel.cs
@@ -1,19 +1,17 @@
 ﻿using CollegeInfoSystem.Models;
 using CollegeInfoSystem.Services;
+using CollegeInfoSystem.ViewModels;
+using CollegeInfoSystem.Views;
 using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
-using System.Threading.Tasks;
-using System.Windows.Input;
-
-namespace CollegeInfoSystem.ViewModels;
 
 public class StaffViewModel : BaseViewModel
 {
     private readonly StaffService _staffService;
-    private Staff _selectedStaff;
 
     public ObservableCollection<Staff> StaffList { get; set; } = new();
 
+    private Staff _selectedStaff;
     public Staff SelectedStaff
     {
         get => _selectedStaff;
@@ -21,21 +19,25 @@ public class StaffViewModel : BaseViewModel
         {
             _selectedStaff = value;
             OnPropertyChanged();
+            ((RelayCommand)UpdateStaffCommand).NotifyCanExecuteChanged(); 
+            ((RelayCommand)DeleteStaffCommand).NotifyCanExecuteChanged(); 
         }
     }
 
-    public ICommand LoadStaffCommand { get; }
-    public ICommand AddStaffCommand { get; }
-    public ICommand UpdateStaffCommand { get; }
-    public ICommand DeleteStaffCommand { get; }
+    public RelayCommand LoadStaffCommand { get; }
+    public RelayCommand AddStaffCommand { get; }
+    public RelayCommand UpdateStaffCommand { get; }
+    public RelayCommand DeleteStaffCommand { get; }
 
     public StaffViewModel(StaffService staffService)
     {
         _staffService = staffService;
         LoadStaffCommand = new RelayCommand(async () => await LoadStaffAsync());
-        AddStaffCommand = new RelayCommand(async () => await AddStaffAsync());
-        UpdateStaffCommand = new RelayCommand(async () => await UpdateStaffAsync(), () => SelectedStaff != null);
-        DeleteStaffCommand = new RelayCommand(async () => await DeleteStaffAsync(), () => SelectedStaff != null);
+        AddStaffCommand = new RelayCommand(AddStaff);
+        UpdateStaffCommand = new RelayCommand(UpdateStaff, () => SelectedStaff != null); 
+        DeleteStaffCommand = new RelayCommand(async () => await DeleteStaffAsync(), () => SelectedStaff != null); 
+
+        Task.Run(async () => await LoadStaffAsync());
     }
 
     public async Task LoadStaffAsync()
@@ -48,17 +50,21 @@ public class StaffViewModel : BaseViewModel
         }
     }
 
-    private async Task AddStaffAsync()
+    private async void AddStaff()
     {
-        var newStaff = new Staff { FirstName = "Новий", LastName = "Працівник", Position = "Посада" };
+        var newStaff = new Staff();
+        OpenStaffDialog(newStaff);
+
         await _staffService.AddStaffAsync(newStaff);
         await LoadStaffAsync();
     }
 
-    private async Task UpdateStaffAsync()
+    private async void UpdateStaff()
     {
         if (SelectedStaff != null)
         {
+            OpenStaffDialog(SelectedStaff);
+
             await _staffService.UpdateStaffAsync(SelectedStaff);
             await LoadStaffAsync();
         }
@@ -71,5 +77,14 @@ public class StaffViewModel : BaseViewModel
             await _staffService.DeleteStaffAsync(SelectedStaff.StaffID);
             await LoadStaffAsync();
         }
+    }
+
+    private void OpenStaffDialog(Staff staff)
+    {
+        var viewModel = new StaffDialogViewModel(staff);
+        var dialog = new StaffDialog { DataContext = viewModel };
+
+        viewModel.CloseAction = () => dialog.Close();
+        dialog.ShowDialog();
     }
 }
